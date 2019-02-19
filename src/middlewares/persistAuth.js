482e561c27bccfc5fc2151a1a9135e68dc8cc189
Jourdi Waller <jourdiwaller@gmail.com>
2019-02-19 10:51:28 +0100

    Factorization of persistAuth

--- src/middlewares/persistAuth.js
+++ src/middlewares/persistAuth.js
@@ -2,13 +2,10 @@ import { AUTH_SUCCESS } from "auth/actions";
 import { AUTH_DELETE } from "../modules/auth/actions";
 
 const persistAuth = store => next => action => {
+  next(action)
   if ([AUTH_SUCCESS, AUTH_DELETE].includes(action.type) && window) {
-    next(action);
     const authState = store.getState().auth;
     localStorage.setItem("auth", JSON.stringify(authState));
-  }
-  else {
-    next(action)
   }
 };
 
