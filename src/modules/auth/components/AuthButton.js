415b29f37ac22770f8fadcab2c83b6fcd18330d4
feychenie <frederik.eychenie@sigfox.com>
2019-02-19 08:09:47 +0100

    feat(auth): logout

--- src/modules/auth/components/AuthButton.js
+++ src/modules/auth/components/AuthButton.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-
-const startAuthentication = () => {
+import { authDelete } from "auth/actions";
+const startLogin = () => {
   window.location.href = `https://github.com/login/oauth/authorize?client_id=cc805ef06019d52139a3&scope=gist,repo`;
 };
 
-const AuthButton = () => <button onClick={startAuthentication}>login</button>;
+const AuthButton = ({ authenticated, logout }) =>
+  authenticated ? (
+    <button onClick={logout}>logout</button>
+  ) : (
+    <button onClick={startLogin}>login</button>
+  );
 
-export default connect()(AuthButton);
+export default connect(
+  state => ({ authenticated: state.auth.authenticated }), // mapState to props
+  dispatch => ({ logout: () => dispatch(authDelete()) })
+)(AuthButton);
